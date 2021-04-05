e1e02233ccaa526cee46886452800fecb952a9eb
Pedro Nascimento <pedrovitor.marques@gmail.com>
2021-04-05 11:51:10 -0300

    Criação de classes; Renderização do Circulo

--- src/basic.renderer.js
+++ src/basic.renderer.js
@@ -1,46 +1,44 @@
+
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
 	(global = global || self, factory(global.BasicRenderer = {}));
 }(this, (function (exports) { 'use strict';
 
 
-        /* ------------------------------------------------------------ */
+/* ------------------------------------------------------------ */
 
-
-        
-    function inside(  x, y, primitive  ) {
-            // You should implement your inside test here for all shapes   
-            // for now, it only returns a false test
-            
-            return false
+       
+    function inside( x, y, primitive) {
+        var circle = new Circle(0, 0, 5);
+        var triangles = circle.triangulate();
+        console.log("T: " + triangles.length)
+        return false
     }
-        
     
     function Screen( width, height, scene ) {
         this.width = width;
         this.height = height;
-        this.scene = this.preprocess(scene);   
+        this.triangles = this.preprocess(scene);   
         this.createImage(); 
     }
 
     Object.assign( Screen.prototype, {
 
             preprocess: function(scene) {
-                // Possible preprocessing with scene primitives, for now we don't change anything
-                // You may define bounding boxes, convert shapes, etc
                 
                 var preprop_scene = [];
 
                 for( var primitive of scene ) {  
-                    // do some processing
-                    // for now, only copies each primitive to a new list
 
-                    preprop_scene.push( primitive );
-                    
+                    if (primitive.shape == "circle"){
+                        var circle = new Circle(primitive);
+                        for (var triangle of circle.triangulate()){
+                            console.log(triangle);
+                            preprop_scene.push( triangle );
+                        }
+                    } 
                 }
-
-                
                 return preprop_scene;
             },
 
@@ -51,23 +49,20 @@
             rasterize: function() {
                 var color;
          
-                // In this loop, the image attribute must be updated after the rasterization procedure.
-                for( var primitive of this.scene ) {
+                for( var triangle of this.triangles ) {
+                    var bounding_box = triangle.get_bounding_box();
 
-                    // Loop through all pixels
-                    // Use bounding boxes in order to speed up this loop
-                    for (var i = 0; i < this.width; i++) {
+                    for (var i = bounding_box['x']['min']; i < bounding_box['x']['max']; i++) {
                         var x = i + 0.5;
-                        for( var j = 0; j < this.height; j++) {
+
+                        for( var j = bounding_box['y']['min']; j < bounding_box['y']['max']; j++) {
                             var y = j + 0.5;
 
-                            // First, we check if the pixel center is inside the primitive 
-                            if ( inside( x, y, primitive ) ) {
-                                // only solid colors for now
-                                color = nj.array(primitive.color);
+                            if ( triangle.is_point_inside(x, y) ) {
+                                console.log("Inside!");
+                                color = nj.array(triangle.color);
                                 this.set_pixel( i, this.height - (j + 1), color );
                             }
-                            
                         }
                     }
                 }
@@ -77,25 +72,129 @@
             },
 
             set_pixel: function( i, j, colorarr ) {
-                // We assume that every shape has solid color
-         
                 this.image.set(j, i, 0,    colorarr.get(0));
                 this.image.set(j, i, 1,    colorarr.get(1));
                 this.image.set(j, i, 2,    colorarr.get(2));
             },
 
             update: function () {
-                // Loading HTML element
                 var $image = document.getElementById('raster_image');
                 $image.width = this.width; $image.height = this.height;
-
-                // Saving the image
                 nj.images.save( this.image, $image );
             }
         }
     );
 
     exports.Screen = Screen;
+
+    class Circle {
+        constructor(primitive_dictionary){
+            this.center_x = primitive_dictionary.center[0];
+            this.center_y = primitive_dictionary.center[1];
+            this.radius = primitive_dictionary.radius;
+            this.color = primitive_dictionary.color;
+        }
+    
+        triangulate(num_slices = 12){
+            var angle_per_slice = 2 * Math.PI / num_slices;
+            
+            var points = [];
+            for (var i = 0; i < num_slices; i++){
+                var theta = i * angle_per_slice;
+                var x = this.radius * Math.cos(theta) + this.center_x;
+                var y = this.radius * Math.sin(theta) + this.center_y;
+                points.push([x,y])
+            }
+    
+            var triangles = [];
+            for (var i = 0; i < num_slices; i++){
+                var center_point = [this.center_x, this.center_y];
+                var current_point = points[i];
+                var next_point = points[(i+1) % num_slices];
+                triangles.push(new Triangle([center_point, current_point, next_point], this.color));
+            }
+            return triangles;
+        }
+    
+        is_point_inside(x, y){
+            return Math.pow(x-this.center_x, 2) + Math.pow(y-this.center_y, 2) <  Math.pow(this.radius, 2);
+        }
+    }
+    
+    class Polygon {
+        constructor(points, color){
+            this.points = points;
+            this.color = color;
+        }
+    
+        triangulate(){
+            // Divide o polígono em uma lista de triangulos (Triangle)
+        }
+    }
+    
+    class Triangle{
+        constructor(points, color){
+            this.points = points;
+            this.color = color;
+        }
+    
+        is_point_inside(x,y){
+            var success = 0;
+    
+            for(var i = 0; i < this.points.length; i++){
+                var currentPoint = this.points[i];
+                var nextPoint = this.points[(i+1) % this.points.length];
+    
+                var firstVector = [x-currentPoint[0], y-currentPoint[1]];
+                var secondVector = [nextPoint[0]-currentPoint[0], nextPoint[1]-currentPoint[1]];
+                var crossProduct = cross_product(firstVector, secondVector);
+
+                if (crossProduct[2] > 0){
+                    success++;
+                }else{
+                    success--;
+                }
+            }
+            return Math.abs(success) == this.points.length;
+        }
+        get_bounding_box(){
+            var min_x = this.points[0][0]
+            var min_y = this.points[0][1];
+            var max_x = this.points[0][0];
+            var max_y = this.points[0][1];
+            for (var point of this.points){
+                if (point[0] > max_x){
+                    max_x = point[0]
+                }else if (point[0] < min_x){
+                    min_x = point[0]
+                }
+                if (point[1] > max_y){
+                    max_y = point[1]
+                }else if (point[1] < min_y){
+                    min_y = point[1]
+                }
+            }
+            var result = {
+                'x' : {
+                    'min' : Math.round(min_x),
+                    'max' : Math.round(max_x)
+                },
+                'y' : {
+                    'min' : Math.round(min_y),
+                    'max' : Math.round(max_y)
+                }
+            };
+            return result;
+        }
+    }
+    
+    function cross_product(firstVector, secondVector){
+        return [
+            firstVector[1] * secondVector[2] - firstVector[2] * secondVector[1],
+            firstVector[2] * secondVector[0] - firstVector[0] * secondVector[2],
+            firstVector[0] * secondVector[1] - firstVector[1] * secondVector[0]
+        ];
+    }
     
 })));
 
